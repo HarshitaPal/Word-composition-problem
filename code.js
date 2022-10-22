812f042722986ed1abdd86ed677bc9e818a66f5f
HarshitaPal <71585192+HarshitaPal@users.noreply.github.com>
2022-10-22 23:51:07 +0530

    Create code.js

---
+++ code.js
@@ -0,0 +1,63 @@
+function TrieNode(key) {
+  this.key = key;
+  this.parent = null;
+  this.children = {};
+  this.end = false;
+}
+Trie.prototype.insert = function (word) {
+  let node = this.root;
+
+  for (let index = 0; index < word.length; index++) {
+    if (!node.children[word[index]]) {
+      node.children[word[index]] = new TrieNode(word[index]);
+      node.children[word[index]].parent = node;
+    }
+    node = node.children[word[index]];
+
+    if (index === word.length - 1) {
+      node.end = true;
+    }
+  }
+};
+TrieNode.prototype.getWord = function () {
+  const output = [];
+  let node = this;
+
+  while (node !== null) {
+    output.unshift(node.key);
+    node = node.parent;
+  }
+
+  return output.join("");
+};
+const findAllWords = (node, arr) => {
+  if (node.end) arr.unshift(node.getWord());
+
+  for (let child in node.children) {
+    findAllWords(node.children[child], arr);
+  }
+};
+Trie.prototype.find = function (prefix) {
+  let node = this.root;
+  const output = [];
+
+  for (let index = 0; index < prefix.length; index++) {
+    if (node.children[prefix[index]]) {
+      node = node.children[prefix[index]];
+    } else {
+      return output;
+    }
+  }
+  findAllWords(node, output);
+
+  return output;
+};
+
+/**
+ * Trie
+ */
+function Trie() {
+  this.root = new TrieNode(null);
+}
+
+module.exports = { Trie };
